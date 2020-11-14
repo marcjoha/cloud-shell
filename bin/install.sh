22f9ab4ec447f18ceade6c369bfa782554d5a79e
Marcus Johansson <majohansson@gmail.com>
2020-11-14 10:35:22 +0000

    fixes

--- bin/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/bin
-
-CWD=`pwd`
-ln -sf $CWD/kube-ps1.sh  ~/bin/kubee-ps1.sh
-ln -sf $CWD/aws-mfa  ~/bin/aws-mfa
--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 CWD=`pwd`
+
+# Set up binaries
+mkdir -p ~/bin
+ln -sf $CWD/kube-ps1.sh  ~/bin/kubee-ps1.sh
+ln -sf $CWD/aws-mfa  ~/bin/aws-mfa
+
+# Symlink all dotfiles
 ln -sf $CWD/customize_environment  ~/.customize_environment
 ln -sf $CWD/bashrc  ~/.bashrc
 ln -sf $CWD/vimrc  ~/.vimrc
