fb9fa2e5eefbfd3d2326e00fe32ec4b1f66713e2
Marcus Johansson <majohansson@gmail.com>
2020-11-14 10:55:05 +0000

    changes

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@ CWD=`pwd`
 
 # Set up binaries
 mkdir -p ~/bin
-ln -sf $CWD/kube-ps1.sh  ~/bin/kubee-ps1.sh
-ln -sf $CWD/aws-mfa  ~/bin/aws-mfa
+ln -sf $CWD/bin/kube-ps1.sh  ~/bin/kube-ps1.sh
+ln -sf $CWD/bin/aws-mfa  ~/bin/aws-mfa
 
 # Symlink all dotfiles
-ln -sf $CWD/customize_environment  ~/.customize_environment
-ln -sf $CWD/bashrc  ~/.bashrc
-ln -sf $CWD/vimrc  ~/.vimrc
-ln -sf $CWD/tmux.conf  ~/.tmux.conf
+ln -sf $CWD/dotfiles/customize_environment  ~/.customize_environment
+chmod a+x $CWD/dotfiles/customize_environment
+ln -sf $CWD/dotfiles/bashrc  ~/.bashrc
+ln -sf $CWD/dotfiles/vimrc  ~/.vimrc
+ln -sf $CWD/dotfiles/tmux.conf  ~/.tmux.conf
