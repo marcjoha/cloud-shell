61a643ecb01f14067ce053a7f750bae84f7423cb
Marcus Johansson <majohansson@gmail.com>
2021-03-10 18:58:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,20 @@ CWD=`pwd`
 # Clean up
 rm ~/README-cloudshell.txt &> /dev/null
 
-# Set up binaries
+# Create base folders
+mkdir -p ~/.cloudshell
 mkdir -p ~/bin
+
+# Remove apt-get warning
+touch ~/.cloudshell/no-apt-get-warning
+
+# Install some binaries
 ln -sf $CWD/bin/kube-ps1.sh  ~/bin/kube-ps1.sh
 chmod a+x $CWD/bin/kube-ps1.sh
 ln -sf $CWD/bin/aws-mfa  ~/bin/aws-mfa
 chmod a+x $CWD/bin/aws-mfa
 
-# Symlink all dotfiles
+# Symlink dotfiles
 ln -sf $CWD/dotfiles/customize_environment  ~/.customize_environment
 chmod a+x $CWD/dotfiles/customize_environment
 ln -sf $CWD/dotfiles/bashrc  ~/.bashrc
